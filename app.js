dabbb4678f8cb8bff58f59b55e5aa177f9180cd9
srikk <122169588+py-code314@users.noreply.github.com>
2026-06-07 19:51:01 -0400

    chore: Install express

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
 import 'dotenv/config'
+import express from 'express'
 
-console.log('Hello ever running Node.js project.')
-console.log(process.env.MY_SECRET)
+/**
+ * -------------- GENERAL SETUP ----------------
+ */
+
+const app = express()
+
+// Express middleware 
+app.use(express.json()) // To read JSON data from client
+app.use(express.urlencoded({ extended: true })) // To parse URL encoded form data
+
+/**
+ * -------------- SERVER ----------------
+ */
+
+// Port to listen on
+const PORT = process.env.PORT || 3000
+app.listen(PORT, (error) => {
+  if (error) {
+    throw error
+  }
+  console.log(`Blog API App - listening on port ${PORT}!`)
+})
+
+// console.log('Hello ever running Node.js project.')
+// console.log(process.env.MY_SECRET)
