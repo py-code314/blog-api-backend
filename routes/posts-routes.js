28ddb694729446f7be52021278572ec5e2ac3ca3
srikk <122169588+py-code314@users.noreply.github.com>
2026-06-17 22:10:23 -0400

    feat: Add post route for a new blog post

--- routes/posts-routes.js
+++ routes/posts-routes.js
@@ -8,5 +8,8 @@ postsRouter.use(isAuth)
 
 // New post routes
 postsRouter.get('/new', controllers.post_get)
+postsRouter.post('/new', (req, res) => {
+  res.send('Post a new blog post')
+})
 
 export { postsRouter }
